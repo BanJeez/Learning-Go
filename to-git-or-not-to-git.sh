832421dc7f8b70e8bed3117adb919106fe49e0ec
Banji <olabanji.ajayi@gmail.com>
2021-09-06 13:31:21 +0100

    2nd commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-#curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"Banji\"}}){id}}"}'
+curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"Banji\"}}){id}}"}'|jq '.[][][].id'
 
-echo 1334
