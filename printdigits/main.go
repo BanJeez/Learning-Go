80abcb92b601c522bed2d8ab9b0f3f3ee44e1cd6
hello <hello@hello.fr>
2021-09-07 15:27:20 +0100

    it works

--- printdigits/main.go
+++ printdigits/main.go
@@ -3,13 +3,15 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var aRune string = "123456789"
+	var aRune string = "00123456789"
 
 	// z01.PrintRune(rune(aRune[i]))
 
-	for i := 0; i <= 8; i++ {
+	for i := 0; i <= 9; {
+		i++
 		// fmt.Println(i)
 		z01.PrintRune(rune(aRune[i]))
+
 	}
 	z01.PrintRune('\n')
 }
