991bc2a3082d492bcdceb7a47c869acbdb392890
Banji <olabanji.ajayi@gmail.com>
2021-09-08 23:11:47 +0100

    going home

--- Test/main.go
+++ Test/main.go
@@ -1,14 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"piscine"
-)
+import "fmt"
 
+// Main Function
 func main() {
-	a := 0
-	b := &a
-	n := &b
-	piscine.UltimatePointOne(&n)
-	fmt.Println(a)
+	for a := 0; a <= 7; a++ {
+		for b := a + 1; b <= 8; b++ {
+			for c := b + 1; c <= 9; c++ {
+				fmt.Println(a, b, c)
+				fmt.Println(",")
+			}
+		}
+	}
 }
