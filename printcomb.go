ee3c36019aafdcad49b0154f7ca393f31626c842
BanJeez <olabanji.ajayi@gmail.com>
2021-09-09 09:51:18 +0100

    formatied

--- printcomb.go
+++ printcomb.go
@@ -3,22 +3,22 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a:= 48 ; a < 56 ; a++{
-		for b:= a + 1 ; b < 57 ; b++{
-			for c:= b + 1 ; c <58 ; c++{
-				if a ==55 && b == 56 && k == 57{
+	for a := 48; a < 56; a++ {
+		for b := a + 1; b < 57; b++ {
+			for c := b + 1; c < 58; c++ {
+				if a == 55 && b == 56 && c == 57 {
 					z01.PrintRune(rune(a))
 					z01.PrintRune(rune(b))
 					z01.PrintRune(rune(c))
 					z01.PrintRune('\n')
-				}else {
+				} else {
 					z01.PrintRune(rune(a))
 					z01.PrintRune(rune(b))
 					z01.PrintRune(rune(c))
 					z01.PrintRune(rune(44))
 					z01.PrintRune(rune(32))
 				}
-			} 
+			}
 		}
 	}
 }
