b734860665c3baf7309966468b056aa81b4249f9
Banji <olabanji.ajayi@gmail.com>
2021-09-09 18:11:27 +0100

    going home 2

---
+++ strrev.go
@@ -0,0 +1,12 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func StrRev(s string) string {
+	string := []rune(s)
+
+	for i := len(s); i >= 0; i-- {
+		// fmt.Println(i)
+	}
+	z01.PrintRune('\n')
+}
