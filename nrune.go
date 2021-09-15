a7dc4698f0e918867ee758a027ba5aeacbf4be95
Banji <olabanji.ajayi@gmail.com>
2021-09-15 15:26:32 +0100

    hahah

--- nrune.go
+++ nrune.go
@@ -3,7 +3,7 @@ package piscine
 func NRune(s string, n int) rune {
 	// var Nrune rune
 	x := []rune(s)
-	if n > len(s) || n < 0 {
+	if n > len(s)-1 || n < 0 {
 		return 0
 	} else {
 		return x[n-1]
