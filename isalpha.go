ea1f6133149d1bae0770ec7f12a5e5ca891a09a1
BanJeez <olabanji.ajayi@gmail.com>
2021-09-16 09:50:20 +0100

    yo

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, x := range s {
-		if x <= 47 || x >= 58 && x <= 64 || x > 90 && x < 97 || x > 122 {
+		if x <= 47 || x >= 58 && x <= 64 || x > 90 && x < 97 || x > 'z' {
 			return false
 		}
 	}
