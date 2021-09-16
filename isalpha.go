a92b8ac30c3b099659d1c110ce8b2e7f143ca8dc
BanJeez <olabanji.ajayi@gmail.com>
2021-09-16 09:15:25 +0100

    lets go

--- isalpha.go
+++ isalpha.go
@@ -2,10 +2,9 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, x := range s {
-		if x <= 47 || x >= 58 && x <= 64 || x > 90 && x < 97 || x > 'z' {
+		if x <= 47 || x >= 58 && x <= 64 || x > 90 && x < 97 || x > 122 {
 			return false
 		}
-
 	}
 	return true
 }
