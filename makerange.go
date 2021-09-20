7a212098e9c9579ac08ae51e983ba1b85377f1e9
Your Name <you@example.com>
2021-09-20 17:36:39 +0100

    yo

---
+++ makerange.go
@@ -0,0 +1,16 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	ii := min
+	var B []int
+
+	if max < min {
+		return B
+	}
+	A := make([]int, max-min)
+	for i := 0; i < max-min; i++ {
+		A[i] = ii
+		ii++
+	}
+	return A
+}
