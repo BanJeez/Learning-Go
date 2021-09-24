f7b21e9af889f724eb38681fefd69ab8dc0c0e95
Your Name <you@example.com>
2021-09-24 10:07:09 +0100

    hahah

---
+++ point/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	// Had to print 1 by 1
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune('4')
+	z01.PrintRune('2')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune('2')
+	z01.PrintRune('1')
+	z01.PrintRune('\n')
+}
